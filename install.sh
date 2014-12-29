0376fb7dac01c8727771691963c1ee59a51a9488
Chris Stokes <stokes84@gmail.com>
2014-12-29 13:28:37 -0800

    Update install.sh
    Major update, adding multi instance support and fixing minor issues.

--- install.sh
+++ install.sh
@@ -1,97 +1,127 @@
 #!/bin/bash
-
-# Last Updated - 12/24/14
+# Last Updated - 12/28/14
 # Chris Stokes - https://github.com/stokes84/Teamspeak-Installer
 #
-# Install
-# wget -q https://raw.githubusercontent.com/stokes84/Teamspeak-Installer/master/install.sh && bash install.sh && rm -f install.sh
+# Install Latest
+# wget -q --no-check-certificate https://raw.githubusercontent.com/stokes84/Teamspeak-Installer/master/install.sh; bash install.sh; rm -f install.sh
 #
 # Usage
-# service teamspeak start
-# service teamspeak stop
-# service teamspeak restart
-# service teamspeak status
+# service teamspeak-yourserver start
+# service teamspeak-yourserver stop
+# service teamspeak-yourserver restart
+# service teamspeak-yourserver status
 
 # Set some styles
 bold=`tput bold`
-alert=`tput setaf 1`
 info=`tput setaf 3`
 normal=`tput sgr0`
 
 # Ensure this script is run as root
 if [ "$(id -u)" != "0" ]; then
-    echo "#\tThis script must be run as root." 1>&2
+    echo "${bold}This script must be run as root${normal}" 1>&2
     exit 1
 fi
 
+# Install variables
+serverdir='/home/teamspeak'
 bit=$(uname -a)
 serverip=$(wget -qO- http://ipecho.net/plain ; echo)
 
-### For future use in setting up multiple instances (remove triples hashes from code below)
-### Will require NPL License @ http://npl.teamspeakusa.com/ts3npl.php
-### Will require within script append all instances of ts3server with ${ts3servername} (e.g. ts3server-${ts3servername})
-### Also append service name and init filename
-if id -u ts3user >/dev/null 2>&1; then
+# Install dependencies
+if [ -f /etc/redhat-release ]; then
+	printf "\n${bold}Installing dependencies...${normal}\n"
+	yum -y -q install lsof
+else
+	printf "${bold}Installing dependencies...${normal}"
+	apt-get -y -q install lsof
+fi
 
-        echo "TS3 system account already configured, skipping to server setup..."
+# Check if user exists in case of previous install or multi instance install
+if id -u teamspeak >/dev/null 2>&1; then
+
+    printf "\n${bold}TeamSpeak 3 service account(teamspeak) already exists${normal}\n"
         
-        # Go to the ts3user directory
-        cd /home/ts3user
-        
-        ### printf "\n${bold}Note:{normal} Alphanumeric and dashes only, everything else will be trimmed.\n"
-        ### read -e -p "Teamspeak 3 Server Name: " -i "server-name" dirtyts3servername
+    # Go to the TeamSpeak directory
+    cd ${serverdir}
+    
+	# Set TeamSpeak server name
+    printf "\n${bold}${info}Note:${normal} Alphanumeric and dashes only, everything else will be trimmed.\n"
+    read -e -p "Teamspeak 3 Server Name: " -i "server-name" inputservername
 	
-	### Only alphanumeric names, we'll make sure of that
-	### export ts3servername="`echo -n "${dirtyts3servername}" | tr -cd '[:alnum:] [:space:]' | tr '[:space:]' '-'  | tr '[:upper:]' '[:lower:]'`"
+	# Only alphanumeric names with dashes, we'll make sure of that
+	servername="`echo -n "${inputservername}" | tr -cd '[:alnum:] [:space:]' | tr '[:space:]' '-'  | tr '[:upper:]' '[:lower:]'`"
 else
 	# Create TS3 user account
-	printf "\n${bold}Creating Teamspeak 3 system account${normal}\n"
-	useradd -d /home/ts3user -m ts3user
+	printf "\n${bold}Creating TeamSpeak 3 service account (teamspeak)${normal}\n"
+	useradd -d ${serverdir} -m teamspeak
 
-	# Set TS3 account password
-	# printf "\n${bold}Creating Teamspeak 3 system account password${normal}\n"
-	# passwd ts3user
-
-	# Set permissions on the new ts3user directory
-	chmod 755 /home/ts3user
-
-	# Go to the ts3user directory
-	cd /home/ts3user
+	# Go to the Teamspeak directory
+	cd ${serverdir}
 	
-	### printf "\n${bold}Note:{normal} Alphanumeric and dashes only, everything else will be trimmed.\n"
-	### read -e -p "Teamspeak 3 Server Name: " -i "server-name" ts3servername
+	# Set TeamSpeak server name
+    printf "\n${bold}${info}Note:${normal} Alphanumeric and dashes only, everything else will be trimmed.\n"
+    read -e -p "TeamSpeak 3 Server Name: " -i "server-name" inputservername
 	
-	### Only alphanumeric names, we'll make sure of that
-	### export cleants3servername="`echo -n "${ts3servername}" | tr -cd '[:alnum:] [:space:]' | tr '[:space:]' '-'  | tr '[:upper:]' '[:lower:]'`"
-
+	# Only alphanumeric names, we'll make sure of that
+	servername="`echo -n "${inputservername}" | tr -cd '[:alnum:] [:space:]' | tr '[:space:]' '-'  | tr '[:upper:]' '[:lower:]'`"
 fi
 
-# Download, unpack, and install the Teamspeak application
+# Function to display download progress bar and hide everything else
+progressfilt ()
+{
+    local flag=false c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c
+    do
+        if $flag
+        then
+            printf '%c' "$c"
+        else
+            if [[ $c != $cr && $c != $nl ]]
+            then
+                count=0
+            else
+                ((count++))
+                if ((count > 1))
+                then
+                    flag=true
+                fi
+            fi
+        fi
+    done
+}
+
+# Download, unpack, and install the TeamSpeak application
 if [[ ${bit} == *x86_64* ]]; then
 	# You're running 64 bit
-	printf "\n${bold}64 bit install running...${normal}\n"
-	wget http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.0.11.2.tar.gz -O teamspeak3-64.tar.gz
+	printf "\n${bold}Downloading latest 64bit version of TeamSpeak 3${normal}\n"
+	wget --progress=bar:force http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-amd64-3.0.11.2.tar.gz -O teamspeak3-64.tar.gz 2>&1 | progressfilt
 	tar xzf teamspeak3-64.tar.gz
 	rm -f teamspeak3-64.tar.gz
-	mv teamspeak3-server_linux-amd64 ts3server
-	cd ts3server
+	# If installer was run previously but did not complete
+	if [ -d "${servername}" ]; then
+		printf "Removing existing directory: ${servername}\n\n"
+		rm -rf ${servername}
+	fi
+	mv teamspeak3-server_linux-amd64 ${servername}
+	cd ${servername}
 	chmod +x ts3server_startscript.sh
-	printf "\nMake sure to copy your ${bold}loginname, password, and token${normal} during the next step"
-	printf "\n${bold}Note:${normal} The installer will not continue until you copy the token (CTRL+C)\n"
-	read -p "Press ${bold}[Enter]${normal} to continue..."
 else 
 	# You're running 32 bit
-	printf "\n${bold}32 bit install running...${normal}\n"
-	wget http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-x86-3.0.11.2.tar.gz -O teamspeak3-32.tar.gz
+	printf "\n${bold}Downloading latest 32bit version of TeamSpeak 3${normal}\n"
+	wget --progress=bar:force http://dl.4players.de/ts/releases/3.0.11.2/teamspeak3-server_linux-x86-3.0.11.2.tar.gz -O teamspeak3-32.tar.gz 2>&1 | progressfilt
 	tar xzf teamspeak3-32.tar.gz
 	rm -f teamspeak3-32.tar.gz
-	mv teamspeak3-server_linux-x86 ts3server
-	cd ts3server
+	# If installer was run previously but did not complete
+	if [ -d "${servername}" ]; then
+		printf "Removing existing directory: ${servername}\n\n"
+		rm -rf ${servername}
+	fi
+	mv teamspeak3-server_linux-x86 ${servername}
+	cd ${servername}
 	chmod +x ts3server_startscript.sh
-	printf "\nMake sure to copy your ${bold}loginname, password, and token${normal} during the next step"
-	printf "\n${bold}Note:${normal} The installer will not continue until you copy the token (CTRL+C)\n"
-	read -p "Press ${bold}[Enter]${normal} to continue..."
 fi
+
+printf "${bold}Generating TeamSpeak 3 config file @ ${serverdir}/${servername}/server.ini${normal}\n\n"
 
 # Create the ini file
 echo "machine_id=
@@ -113,101 +143,130 @@ logpath=logs
 logquerycommands=0
 dbclientkeepdays=30
 logappend=0
-query_skipbruteforcecheck=0" >> /home/ts3user/ts3server/ts3server.ini
+query_skipbruteforcecheck=0" >> ${serverdir}/${servername}/server.ini
 
-# Change machine_id to 1
-sed -i -e "s|machine_id=|machine_id=1|g" /home/ts3user/ts3server/ts3server.ini
+# Insert machine id into the machine_id field
+printf "${info}${bold}Note:${normal} Leave this blank unless you know what you're doing.\n"
+read -e -p "TeamSpeak 3 Machine ID: " -i "" machineid
+sed -i -e "s|machine_id=|machine_id=$machineid|g" ${serverdir}/${servername}/server.ini
 
-# Insert this machine's IP into the voice_ip field
-sed -i -e "s|voice_ip=0.0.0.0|voice_ip=$serverip|g" /home/ts3user/ts3server/ts3server.ini
+# Double check we're grabbing the IP you want
+printf "\n${info}${bold}Note:${normal} If installing on a machine with a dynamic IP use 0.0.0.0\n"
 
-# Insert this machine's IP into the filetransfer_ip field
-sed -i -e "s|filetransfer_ip=0.0.0.0|filetransfer_ip=$serverip|g" /home/ts3user/ts3server/ts3server.ini
+# Insert IP into the voice_ip field
+read -e -p "TeamSpeak 3 Voice IP: " -i "$serverip" voiceip
+sed -i -e "s|voice_ip=0.0.0.0|voice_ip=$voiceip|g" ${serverdir}/${servername}/server.ini
 
-# Insert this machine's IP into the query_ip field
-sed -i -e "s|query_ip=0.0.0.0|query_ip=$serverip|g" /home/ts3user/ts3server/ts3server.ini
+# Insert IP into the filetransfer_ip field
+read -e -p "TeamSpeak 3 File Transfer IP: " -i "$serverip" fileip
+sed -i -e "s|filetransfer_ip=0.0.0.0|filetransfer_ip=$fileip|g" ${serverdir}/${servername}/server.ini
+
+# Insert IP into the query_ip field
+read -e -p "TeamSpeak 3 Query IP: " -i "$serverip" queryip
+sed -i -e "s|query_ip=0.0.0.0|query_ip=$queryip|g" ${serverdir}/${servername}/server.ini
 
 # Edits the startup script to load the ini file
-sed -i 's|COMMANDLINE_PARAMETERS="${2}"|COMMANDLINE_PARAMETERS="${2} inifile=ts3server.ini"|g' /home/ts3user/ts3server/ts3server_startscript.sh
+sed -i 's|COMMANDLINE_PARAMETERS="${2}"|COMMANDLINE_PARAMETERS="${2} inifile=server.ini"|g' ${serverdir}/${servername}/ts3server_startscript.sh
 
-read -e -p "Teamspeak 3 Server Voice Port: " -i "9987" ts3voiceport
-sed -i -e "s|default_voice_port=9987|default_voice_port=$ts3voiceport|g" /home/ts3user/ts3server/ts3server.ini
+# Set Teamspeak 3 voice port & make sure it's not being used
+while read -e -p "TeamSpeak 3 Server Voice Port: " -i "9987" ts3voiceport; do
+	if [[ -z $(lsof -i :${ts3voiceport}) ]]; then
+		sed -i -e "s|default_voice_port=9987|default_voice_port=$ts3voiceport|g" ${serverdir}/${servername}/server.ini
+		break
+	else 
+		printf "${bold}Port ${ts3voiceport} in use, try another port\n${normal}"
+	fi
+done
 
-read -e -p "Teamspeak 3 Server File Transfer Port: " -i "30033" ts3fileport
-sed -i -e "s|filetransfer_port=30033|filetransfer_port=$ts3fileport|g" /home/ts3user/ts3server/ts3server.ini
+# Set Teamspeak 3 server file transfer port & make sure it's not being used
+while read -e -p "TeamSpeak 3 Server File Transfer Port: " -i "30033" ts3fileport; do
+	if [[ -z $(lsof -i :${ts3fileport}) ]]; then
+		sed -i -e "s|filetransfer_port=30033|filetransfer_port=$ts3fileport|g" ${serverdir}/${servername}/server.ini
+		break
+	else 
+		printf "${bold}Port ${ts3fileport} use, try another port\n${normal}"
+	fi
+done
 
-read -e -p "Teamspeak 3 Server Query Port: " -i "10011" ts3queryport
-sed -i -e "s|query_port=9987|query_port=$ts3queryport|g" /home/ts3user/ts3server/ts3server.ini
+# Set Teamspeak 3 server query port & make sure it's not being used
+while read -e -p "TeamSpeak 3 Server Query Port: " -i "10011" ts3queryport; do
+	if [[ -z $(lsof -i :${ts3queryport}) ]]; then
+		sed -i -e "s|query_port=9987|query_port=$ts3queryport|g" ${serverdir}/${servername}/server.ini
+		break
+	else 
+		printf "${bold}Port ${ts3queryport} in use, try another port\n${normal}"
+	fi
+done
 
-printf "\n${bold}Creating Teamspeak 3 service file${normal}\n"
-
-# Setup the Teamspeak service file
+# Setup the TeamSpeak service file
 if [ -f /etc/redhat-release ]; then
 	echo "#!/bin/sh
 	# chkconfig: 2345 95 20
-	# description: Control TeamSpeak3 Server
-	# processname: /home/ts3user/ts3server/ts3server_runscript.sh
-	cd /home/ts3user/ts3server
+	# description: TeamSpeak 3 Server
+	# processname: ${serverdir}/${servername}/ts3server_startscript.sh
+	cd ${serverdir}/${servername}
 	case \"\$1\" in
 	'start')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh start\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh start\"
 	;;
 	'stop')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh stop\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh stop\"
 	;;
 	'restart')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh restart\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh restart\"
 	;;
 	'status')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh status\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh status\"
 	;;
 	*)
-	echo \"Usage \$0 start|stop|restart|status\"
-	esac" > /etc/rc.d/init.d/teamspeak
+	echo \"Usage: teamspeak-${servername} start|stop|restart|status\"
+	esac" > /etc/rc.d/init.d/teamspeak-${servername}
 else
 	echo "#!/bin/sh
 	# chkconfig: 2345 95 20
-	# description: Control TeamSpeak3 Server
-	# processname: /home/ts3user/ts3server/ts3server_runscript.sh
-	cd /home/ts3user/ts3server
+	# description: TeamSpeak 3 Server
+	# processname: ${serverdir}/${servername}/ts3server_startscript.sh
+	cd ${serverdir}/${servername}
 	case \"\$1\" in
 	'start')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh start\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh start\"
 	;;
 	'stop')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh stop\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh stop\"
 	;;
 	'restart')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh restart\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh restart\"
 	;;
 	'status')
-	su ts3user -c \"/home/ts3user/ts3server/ts3server_startscript.sh status\"
+	su teamspeak -c \"${serverdir}/${servername}/ts3server_startscript.sh status\"
 	;;
 	*)
-	echo \"Usage \$0 start|stop|restart|status\"
-	esac" > /etc/init.d/teamspeak
+	echo \"Usage: teamspeak-${servername} start|stop|restart|status\"
+	esac" > /etc/init.d/teamspeak-${servername}
 fi
 
-# Change permissions and ownership on the teamspeak files
-chown -R ts3user:ts3user /home/ts3user
-chmod +x /home/ts3user/ts3server/ts3server_startscript.sh
+# Change permissions and ownership on the TeamSpeak files
+chown -R teamspeak:teamspeak ${serverdir}
+chmod +x ${serverdir}/${servername}/ts3server_startscript.sh
 
 # Fixing common error @ http://forum.teamspeak.com/showthread.php/68827-Failed-to-register-local-accounting-service
-# check if tmpfs is mounted, add entry to fstab if necessary
 if ! mount|grep -q "/dev/shm"; then
+	echo "tmpfs /dev/shm tmpfs defaults 0 0" >> /etc/fstab
 	mount -t tmpfs tmpfs /dev/shm
-	grep -q "/dev/shm" /etc/fstab || echo "tmpfs /dev/shm tmpfs rw 0 0" >> /etc/fstab
 fi
 
-# Initiate the Teamspeak service and boot at startup
+# Initiate the TeamSpeak service and boot at startup
 if [ -f /etc/redhat-release ]; then
-	chmod +x /etc/rc.d/init.d/teamspeak
-	chkconfig --add teamspeak
-	chkconfig --level 2345 teamspeak on
+	chmod +x /etc/rc.d/init.d/teamspeak-${servername}
+	chkconfig --add teamspeak-${servername}
+	chkconfig --level 2345 teamspeak-${servername} on
 else
-	chmod +x /etc/init.d/teamspeak
-	update-rc.d teamspeak defaults
+	chmod +x /etc/init.d/teamspeak-${servername}
+	update-rc.d teamspeak-${servername} defaults
 fi
-service teamspeak start
-printf "\n${bold}Install Complete!${normal}\n"
-printf "\n${bold}Teamspeak 3 is running @ $serverip:$ts3voiceport${normal}\n"
+
+service teamspeak-${servername} start
+sleep 3
+printf "\nInstall Complete"
+printf "\nTeamSpeak 3 is running @ ${bold}$serverip:$ts3voiceport${normal}"
+printf "\n${bold}Usage:${normal} service teamspeak-${servername} start|stop|restart|status\n"
