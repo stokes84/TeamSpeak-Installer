42a92a7fd18957d33ed8cf75c8c1e2fe4c896f7d
Chris Stokes <stokes84@gmail.com>
2014-12-29 23:45:43 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -237,7 +237,7 @@ else
 	### BEGIN INIT INFO
 	# Provides:          ${serverdir}/${servername}/ts3server_startscript.sh
 	# Required-Start:    $local_fs $network
-	# Required-Stop:     
+	# Required-Stop:     $local_fs $network
 	# Default-Start:     2 3 4 5
 	# Default-Stop:      0 1 6
 	# Short-Description: TeamSpeak 3 Server
